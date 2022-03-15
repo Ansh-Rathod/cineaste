646167e818bf41bd2f29974ffa035dfca480e215
Ansh-Rathod <anshrathod29@gmail.com>
2022-03-15 16:17:51 +0530

    updated user

--- db.js
+++ db.js
@@ -3,20 +3,20 @@ import pg from 'pg'
 const Pool = pg.Pool
 
 // to use ssl database from url
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false,
-	},
-})
+// const pool = new Pool({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: {
+// 		rejectUnauthorized: false,
+// 	},
+// })
 
 // // to use local database
-// const pool = new Pool({
-// 	database: 'postgres',
-// 	host: 'localhost',
-// 	port: 5432,
-// 	user: 'postgres',
-// 	password: 'ansh00',
-// })
+const pool = new Pool({
+	database: 'postgres',
+	host: 'localhost',
+	port: 5432,
+	user: 'ansh',
+	password: 'ansh00',
+})
 
 export default pool
