a072080ef7a04438243676ff72ef13db18de3397
Ansh-Rathod <anshrathod29@gmail.com>
2022-04-07 19:56:32 +0530

    added reviews

--- routes/web/web.js
+++ routes/web/web.js
@@ -48,5 +48,35 @@ router.get(
 		})
 	})
 )
+router.get(
+	'/user/:id',
+	asyncHandler(async (req, res, next) => {
+		const { id } = req.params
+		const { rows } = await pool.query(
+			`select display_name,username,bio,avatar_url from users where username=$1;`,
+
+			[id]
+		)
+		res.status(200).send({
+			success: true,
+			results: rows,
+		})
+	})
+)
+router.get(
+	'/reviews/:id',
+	asyncHandler(async (req, res, next) => {
+		const { id } = req.params
+		const { rows } = await pool.query(
+			`select display_name,username,bio,avatar_url from users where username=(select creator_username from reviews where id=$1);`,
+
+			[id]
+		)
+		res.status(200).send({
+			success: true,
+			results: rows,
+		})
+	})
+)
 
 export default router
