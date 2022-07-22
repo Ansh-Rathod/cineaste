0568f6cb57701a7ca043719d6eb936d78455d6f4
Ansh-Rathod <anshrathod29@gmail.com>
2022-07-22 14:16:40 +0530

    changed database setting

--- db.js
+++ db.js
@@ -6,24 +6,24 @@ const Pool = pg.Pool
 
 
 
-// // digital ocean database
-// const pool = new Pool({
-//   database: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   user: 'postgres',
-//   password: 'ansh00',
-// })
-
-
-
-// direct database
+// digital ocean database
 const pool = new Pool({
-  database: process.env.SERVER_DB_NAME,
-  host: process.env.SERVER_HOST,
-  port: process.env.SERVER_PORT,
-  user: process.env.SERVER_USER_NAME,
-  password: process.env.SERVER_PASS,
+  database: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'ansh00',
 })
+
+
+
+// // direct database
+// const pool = new Pool({
+//   database: process.env.SERVER_DB_NAME,
+//   host: process.env.SERVER_HOST,
+//   port: process.env.SERVER_PORT,
+//   user: process.env.SERVER_USER_NAME,
+//   password: process.env.SERVER_PASS,
+// })
 
 export default pool
